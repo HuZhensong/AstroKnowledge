c2f9f680b419fa81406b31ec7260cc913a6d5e77
HuZhensong <zhensong@sas.upenn.edu>
2020-03-27 11:56:33 +0800

    BasicFrameWork

---
+++ MultipleDatabase.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 27 10:53:06 2020
+
+@author: Z. H
+
+Reference:
+https://www.runoob.com/sqlite/sqlite-delete.htmls
+"""
+
+import sqlite3
+
+dbname = 'Knowledge.db'
+
+def CreateDatabase(dbname='Knowledge.db'):
+    conn = sqlite3.connect(dbname)
+    c = conn.cursor()
+    c.execute('''CREATE TABLE KNOWLEDGE (ID TEXT PRIMARY KEY NOT NULL, DESCRIPTION TEXT NOT NULL, LINK TEXT NOT NULL, OTHER TEXT NOT NULL);''')
+    conn.commit()
+    print ("Table created successfully")
+    conn.close()
+    return
+
+def insert(ID,description, link,dbname='Knowledge.db',other='-'):
+    conn = sqlite3.connect(dbname)
+    c = conn.cursor()
+    c.execute("INSERT INTO KNOWLEDGE (ID,DESCRIPTION,LINK,OTHER) \
+              VALUES('"+ID+"','"+description+"','"+link+"','-')")
+    conn.commit() 
+    #print ("Records created successfully")
+    conn.close()
+    
+    IDfile=open('ID.txt','a')
+    IDfile.write(ID+'\n')
+    IDfile.close()
+    return
+
+def select_all(dbname='Knowledge.db'):
+    conn = sqlite3.connect(dbname)
+    c = conn.cursor()
+    cursor = c.execute("SELECT id, description, link, other from KNOWLEDGE")
+    for row in cursor:
+        print("ID: ",row[0])
+        print("Description: ",row[1])
+        print("Link: ",row[2])
+        print("other: ",row[3])
+        print('\n')
+    conn.close()
+    return
+
+
+def update(ID, upcol,upcontent,dbname='Knowledge.db'):
+    """
+    Which ID, which upcol to update. upcontent is the replaced content.
+    """
+    conn = sqlite3.connect(dbname)
+    c = conn.cursor()
+    c.execute("UPDATE KNOWLEDGE set "+upcol+" = '"+upcontent+"' where ID='"+ID+"'")
+    conn.commit()
+    conn.close()
+    return
+
+def delete(ID,dbname='Knowledge.db'):
+    conn = sqlite3.connect(dbname)
+    c = conn.cursor()
+    c.execute("DELETE from KNOWLEDGE where ID='"+ID+"';")
+    conn.commit()
+    conn.close()
+    return
+
+def like(keyword,dbname='Knowledge.db'):
+    conn = sqlite3.connect(dbname)
+    c = conn.cursor()
+    cursor = c.execute("SELECT * FROM KNOWLEDGE WHERE ID LIKE '%"+keyword+"%';")
+    #print("SELECT * FROM KNOWLEDGE WHERE ID LIKE '%"+keyword+"%'")
+    for row in cursor:
+        print("ID: ",row[0])
+        print("Description: ",row[1])
+        print("Link: ",row[2])
+        print("other: ",row[3])
+        print('\n')
+    conn.close()
+    return
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # AstroKnowledge
  Knowledge collected from papers and books. Easy to look up the concept and source.
+
+ This database framework based on python sqlite3.
+
+ It contains a database named Knowledge.db, with four columns ID, DESCRIPTION, LINK, OTHER. A python script called MultipleDatabase.py is provided for inserting, updating, deleting operations. All of these operations are written in function forms, which is easy to use. Each time you run Insert function in MultipleDatabase.py, the value of ID will be recorded in ID.txt, so that you can look up what's inside this database. 
+
